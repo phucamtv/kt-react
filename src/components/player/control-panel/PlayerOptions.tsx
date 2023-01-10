f45323c2e1645c0c9f6c40ef88a839492fa5bc48
Andy Truong <thehongtt@gmail.com>
2023-01-11 05:23:46 +1000

    SpeedOption controller

--- src/components/player/control-panel/PlayerOptions.tsx
+++ src/components/player/control-panel/PlayerOptions.tsx
@@ -1,9 +1,10 @@
-import { Box, Grid, IconButton } from "@mui/material";
+import { Box, Grid, IconButton, Menu, MenuItem } from "@mui/material";
 import ToggleOffIcon from "@mui/icons-material/ToggleOff";
 import SlowMotionVideoIcon from "@mui/icons-material/SlowMotionVideo";
 import LoopIcon from "@mui/icons-material/Loop";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import React from "react";
+import { useAppState } from "../../app/store";
 
 export const PlayerOptions = () => {
     return <>
@@ -35,10 +36,45 @@ const ButtonToggleOption = () => {
 };
 
 const SpeedOption = () => {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => setAnchorEl(event.currentTarget);
+    const handleClose = () => setAnchorEl(null);
+    
+    const setter = useAppState(state => state.audioPlayer.setSpeed);
+    const value = useAppState(state => state.audio.speed);
+    const onClick = (value: number) => {
+        return () => {
+            handleClose();
+            setter(value);
+        }
+    };
+    
     return <Grid item xs={3} display={"flex"} justifyContent={"center"}>
-        <IconButton onClick={() => console.log}>
-            <SlowMotionVideoIcon /> 1x
+        <IconButton
+            aria-controls={open ? 'basic-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
+        >
+            <SlowMotionVideoIcon /> {value}x
         </IconButton>
+        
+        <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+                'aria-labelledby': 'basic-button',
+            }}
+        >
+            <MenuItem onClick={onClick(0.75)}>0.75x</MenuItem>
+            <MenuItem onClick={onClick(1)}>1x</MenuItem>
+            <MenuItem onClick={onClick(1.25)}>1.25x</MenuItem>
+            <MenuItem onClick={onClick(1.5)}>1.5x</MenuItem>
+            <MenuItem onClick={onClick(2)}>2x</MenuItem>
+        </Menu>
     </Grid>;
 };
 
